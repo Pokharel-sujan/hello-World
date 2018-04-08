b699e3cbf85b304cca4c886f77fae9fe69da0c38
Codeater7 <pokharelsujan134@gmail.com>
2018-04-08 13:05:08 +0300

    Update and rename test.txt to test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+hey my name is sujan. I am an code eater and I am just updating something here to refresh what I have learned so far.
+
+just using to learn it
+function HelloWorld(props){
+  return <h1>Value : {props.numberArray[props.index]} </h1>
+}
+
+ReactDOM.render(
+  <HelloWorld index = '0' numberArray= {[1,2,3,4,5]}/>,
+  
+    document.getElementById('root')
+ )
+  
